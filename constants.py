cc581d76f5124c083d3850ea0d58ed890aed4aa5
Ype Zijlstra <y.zijlstr@gmail.com>
2021-06-08 23:44:54 +0200

    Created groups dictionary

---
+++ constants.py
@@ -0,0 +1,9 @@
+# De verdeling van deelnemende landen over groepen
+groepen = {
+    'A': {'Italy', 'Turkey', 'Wales', 'Switzerland'},
+    'B': {'Belgium', 'Denmark', 'Finland', 'Russia'},
+    'C': {'Netherlands', 'North-Macedonia', 'Ukraine', 'Austria'},
+    'D': {'England', 'Croatia', 'Scotland', 'Czechia'},
+    'E': {'Poland', 'Slowakia', 'Spain', 'Sweden'},
+    'F': {'Germany', 'France', 'Hungary', 'Portugal'}
+}
