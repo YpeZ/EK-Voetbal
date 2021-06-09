d8001099027c581c1dbcda1283fba2ef14e77b2b
Ype Zijlstra <y.zijlstr@gmail.com>
2021-06-09 19:01:47 +0200

    Fixed country names in accordance with the matches database

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@
 groepen = {
     'A': {'Italy', 'Turkey', 'Wales', 'Switzerland'},
     'B': {'Belgium', 'Denmark', 'Finland', 'Russia'},
-    'C': {'Netherlands', 'North-Macedonia', 'Ukraine', 'Austria'},
-    'D': {'England', 'Croatia', 'Scotland', 'Czechia'},
+    'C': {'Netherlands', 'North Macedonia', 'Ukraine', 'Austria'},
+    'D': {'England', 'Croatia', 'Scotland', 'Czech Republic'},
     'E': {'Poland', 'Slovakia', 'Spain', 'Sweden'},
     'F': {'Germany', 'France', 'Hungary', 'Portugal'}
 }
