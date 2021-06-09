ae7283b6a32be128aa96650be88a6af2ef0734b3
Ype Zijlstra <y.zijlstr@gmail.com>
2021-06-10 01:12:06 +0200

    Changed groepen to list instead of dict

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # De verdeling van deelnemende landen over groepen
 groepen = {
-    'A': {'Turkey', 'Italy', 'Wales', 'Switzerland'},
-    'B': {'Denmark', 'Finland', 'Belgium', 'Russia'},
-    'C': {'Austria', 'North Macedonia', 'Netherlands', 'Ukraine'},
-    'D': {'England', 'Croatia', 'Scotland', 'Czech Republic'},
-    'E': {'Poland', 'Slovakia', 'Spain', 'Sweden'},
-    'F': {'Hungary', 'Portugal', 'France', 'Germany'}
+    'A': ['Turkey', 'Italy', 'Wales', 'Switzerland'],
+    'B': ['Denmark', 'Finland', 'Belgium', 'Russia'],
+    'C': ['Austria', 'North Macedonia', 'Netherlands', 'Ukraine'],
+    'D': ['England', 'Croatia', 'Scotland', 'Czech Republic'],
+    'E': ['Poland', 'Slovakia', 'Spain', 'Sweden'],
+    'F': ['Hungary', 'Portugal', 'France', 'Germany']
 }
