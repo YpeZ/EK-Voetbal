4d6d33195c576599a381e6a555ef591fd5b99af1
Ype Zijlstra <y.zijlstr@gmail.com>
2021-06-10 00:28:14 +0200

    Changed order of groups for fixture creation

--- constants.py
+++ constants.py
@@ -1,9 +1,9 @@
 # De verdeling van deelnemende landen over groepen
 groepen = {
-    'A': {'Italy', 'Turkey', 'Wales', 'Switzerland'},
-    'B': {'Belgium', 'Denmark', 'Finland', 'Russia'},
-    'C': {'Netherlands', 'North Macedonia', 'Ukraine', 'Austria'},
+    'A': {'Turkey', 'Italy', 'Wales', 'Switzerland'},
+    'B': {'Denmark', 'Finland', 'Belgium', 'Russia'},
+    'C': {'Austria', 'North Macedonia', 'Netherlands', 'Ukraine'},
     'D': {'England', 'Croatia', 'Scotland', 'Czech Republic'},
     'E': {'Poland', 'Slovakia', 'Spain', 'Sweden'},
-    'F': {'Germany', 'France', 'Hungary', 'Portugal'}
+    'F': {'Hungary', 'Portugal', 'France', 'Germany'}
 }
