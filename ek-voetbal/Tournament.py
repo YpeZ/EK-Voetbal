cf2005b173fd413b9726d8a41fd59aa872a527a3
Ype Zijlstra <y.zijlstr@gmail.com>
2021-06-16 09:07:18 +0200

    Tournament methods return list of match objects. Added print_results method

--- ek-voetbal/Tournament.py
+++ ek-voetbal/Tournament.py
@@ -4,21 +4,39 @@ from Match import Match
 from utils import get_third_place_order
 
 
-class Tournament():
+class Tournament:
     """
     Class for simulating an entire tournament
     """
 
-    def __init__(self):
-        self.groups = {group_idx: Group(group_idx) for group_idx in groepen.keys()}
-        self.group_results = {group_idx: self.groups[group_idx].standings for group_idx in self.groups.keys()}
-        self.group_lists = {group_idx: list(self.group_results[group_idx].keys())
-                            for group_idx in self.group_results.keys()}
+    def __init__(self, id: int = 0, test=False):
+        self.groups = self.create_groups()
+
+        self.group_results = {group_idx: group.standings for (group_idx, group) in self.groups.items()}
+        self.group_lists = {group_idx: group.ranking for (group_idx, group) in self.groups.items()}
         self.third_place_ranked = self.third_places()
-        self.quarter_finalists = self.round_of_16()
-        self.semi_finalists = self.quarter_finals()
-        self.finalists = self.semi_finals()
-        self.winner = self.final()
+
+        self.round_of_16ers = self.second_rounders()
+        self.round_of_16 = self.round_of_16()
+
+        self.quarter_finalists = [match.winner for match in self.round_of_16]
+        self.quarter_finals = self.quarter_finals()
+
+        self.semi_finalists = [match.winner for match in self.quarter_finals]
+        self.semi_finals = self.semi_finals()
+
+        self.finalists = [match.winner for match in self.semi_finals]
+        self.final = self.final()
+
+        self.winner = self.final.winner
+
+    def create_groups(self) -> dict:
+        """
+        Initialize the group stage
+        :return: dictionary containing Group objects indexed by their group names
+        """
+        group_dict = {group_idx: Group(group_idx) for group_idx in groepen.keys()}
+        return group_dict
 
     def third_places(self):
         third_placed = {group_idx: self.group_lists[group_idx][2]
@@ -28,9 +46,10 @@ class Tournament():
                                            'results': self.group_results[group_idx][third_placed[group_idx]]}
                                for group_idx in self.group_results}
 
-        third_placed_points = dict(sorted(third_placed_points.items(), key=lambda tup: (tup[1]['results']['PTS'],
-                                                                                        tup[1]['results']['GD'],
-                                                                                        tup[1]['results']['G']),
+        third_placed_points = dict(sorted(third_placed_points.items(),
+                                          key=lambda tup: (tup[1]['results']['PTS'],
+                                                           tup[1]['results']['GD'],
+                                                           tup[1]['results']['G']),
                                           reverse=True))
 
         third_placed_ranked = {group_idx: third_placed_points[group_idx]['country']
@@ -38,13 +57,27 @@ class Tournament():
 
         return third_placed_ranked
 
+    def second_rounders(self):
+        """
+        Gather a list of all teams that play the round of 16
+        :return: list of teams
+        """
+
+        best_two_teams = [team for teams in self.group_lists.values() for team in teams[:2]]
+        best_third_teams = list(self.third_place_ranked.values())[:4]
+
+        sixteen_best_teams = best_two_teams + best_third_teams
+
+        return sixteen_best_teams
+
     def round_of_16(self):
         """
         Simulate the second round of the tournament based on the group stage results
         :return: list of round of sixteen winners
         """
-        group_lists = {group_idx: list(self.group_results[group_idx].keys())
-                       for group_idx in self.group_results.keys()}
+        # group_lists = {group_idx: list(self.group_results[group_idx].keys())
+        #                for group_idx in self.group_results.keys()}
+        group_lists = self.group_lists
 
         third_placed_list = get_third_place_order(self.third_place_ranked)
 
@@ -62,7 +95,7 @@ class Tournament():
         matches = [Match(matchup[0], matchup[1], extra_time=True) for matchup in matchups]
         ro_16_winners = [match.winner for match in matches]
 
-        return ro_16_winners
+        return matches
 
     def quarter_finals(self):
         """
@@ -82,7 +115,7 @@ class Tournament():
 
         qf_winners = [match.winner for match in qf_matches]
 
-        return qf_winners
+        return qf_matches
 
     def semi_finals(self):
         """
@@ -100,7 +133,7 @@ class Tournament():
 
         sf_winners = [match.winner for match in sf_matches]
 
-        return sf_winners
+        return sf_matches
 
     def final(self):
         """
@@ -111,6 +144,29 @@ class Tournament():
 
         final_match = Match(self.finalists[0], self.finalists[1], extra_time=True)
 
-        return final_match.winner
+        return final_match
 
-tour = Tournament()
+    def print_results(self):
+        print("Group stage")
+        for (key, group) in self.groups.items():
+            print(f"Group {key}")
+            group.print_results()
+            print('\n')
+
+        print("Round of 16")
+        for match in self.round_of_16:
+            match.print_result()
+        print('\n')
+
+        print("Quarter finals")
+        for match in self.quarter_finals:
+            match.print_result()
+        print('\n')
+
+        print("Semi finals")
+        for match in self.semi_finals:
+            match.print_result()
+        print('\n')
+
+        print("Final")
+        self.final.print_result()
