70174be6b388f47c07c0e565daf81c51af153ddc
Ype Zijlstra <y.zijlstr@gmail.com>
2021-06-23 19:14:21 +0200

    Typed out group stage matches as constants values

--- ek-voetbal/constants_values.py
+++ ek-voetbal/constants_values.py
@@ -7,3 +7,54 @@ groepen = {
     'E': ['Poland', 'Slovakia', 'Spain', 'Sweden'],
     'F': ['Hungary', 'Portugal', 'France', 'Germany']
 }
+
+group_matches = {
+    'A': [
+        ['Turkey', 'Italy'],
+        ['Wales', 'Switzerland'],
+        ['Turkey', 'Wales'],
+        ['Italy', 'Switzerland'],
+        ['Switzerland', 'Turkey'],
+        ['Italy', 'Wales']
+    ],
+    'B': [
+        ['Denmark', 'Finland'],
+        ['Belgium', 'Russia'],
+        ['Finland', 'Russia'],
+        ['Denmark', 'Belgium'],
+        ['Russia', 'Denmark'],
+        ['Finland', 'Belgium']
+    ],
+    'C': [
+        ['Austria', 'North Macedonia'],
+        ['Netherlands', 'Austria'],
+        ['Ukraine', 'North Macedonia'],
+        ['Netherlands', 'Austria'],
+        ['North Macedonia', 'Netherlands'],
+        ['Ukraine', 'Austria']
+    ],
+    'D': [
+        ['England', 'Croatia'],
+        ['Scotland', 'Czech Republic'],
+        ['Croatia', 'Czech Republic'],
+        ['England', 'Scotland'],
+        ['Croatia', 'Scotland'],
+        ['Czech Republic', 'England']
+    ],
+    'E': [
+        ['Poland', 'Slovakia'],
+        ['Spain', 'Sweden'],
+        ['Sweden', 'Slovakia'],
+        ['Spain', 'Poland'],
+        ['Slovakia', 'Spain'],
+        ['Sweden', 'Poland']
+    ],
+    'F': [
+        ['Hungary', 'Portugal'],
+        ['France', 'Germany'],
+        ['Hungary', 'France'],
+        ['Portugal', 'Germany'],
+        ['Portugal', 'France'],
+        ['Germany', 'Hungary']
+    ]
+}
